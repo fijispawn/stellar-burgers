e8ae0df5d9c2ee3a682275c845848b0efd7606fb
Mikhail Smirnov <m2004029@edu.misis.ru>
2025-05-08 16:50:35 +0300

    детали об ингредиенте работают

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,17 @@
 import { FC } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from '../../services/store';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { selectIngredients } from '../../services/slices/ingredientsSlice';
+import { TIngredient } from '@utils-types';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { id } = useParams();
+  const ingredients = useSelector(selectIngredients);
+  const ingredientData = ingredients.find(
+    (item: TIngredient) => item._id === id
+  );
 
   if (!ingredientData) {
     return <Preloader />;
